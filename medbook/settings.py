929f2287ba10731fa904287dc47606f534eb7d4a
MafoudohEmmanuelle <mafoudohemmanuelle@gmail.com>
2025-11-03 13:14:18 +0100

    Add static root for deployment

--- medbook/settings.py
+++ medbook/settings.py
@@ -142,10 +142,9 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
