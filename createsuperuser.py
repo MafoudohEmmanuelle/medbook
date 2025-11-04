4923c42f09a2d867c409290849739725c97ceb83
MafoudohEmmanuelle <mafoudohemmanuelle@gmail.com>
2025-11-04 07:37:52 +0100

    Add script to create admin automatically

--- createsuperuser.py
+++ createsuperuser.py
@@ -8,12 +8,17 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        password=password,
+        first_name="Admin",
+        last_name="User",
+        role="ADMIN"  
+    )
     print("✅ Superuser created successfully.")
 else:
     print("⚠️ Superuser already exists.")
